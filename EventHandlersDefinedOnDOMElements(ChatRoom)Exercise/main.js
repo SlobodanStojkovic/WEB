a5509cc4a8abbf0fbfd8203b1e37683895e29f02
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-06-11 22:28:10 +0200

    Chat Room Exercise added function to send message by pressing ENTER

--- EventHandlersDefinedOnDOMElements(ChatRoom)Exercise/main.js
+++ EventHandlersDefinedOnDOMElements(ChatRoom)Exercise/main.js
@@ -5,6 +5,18 @@ var input = document.querySelector(".chatInput");
 buttonHandler.addEventListener("click", function () {
     var paragraph = document.createElement("P");
     paragraph.innerHTML = "" + input.value;
-    document.getElementById("chat").appendChild(paragraph);
+    chat.appendChild(paragraph);
     input.value = "";
+});
+
+
+//this makes message to append when we press ENTER vvv 
+
+input.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+        var paragraph = document.createElement("P");
+        paragraph.innerHTML = "" + input.value;
+        chat.appendChild(paragraph);
+        input.value = "";
+    }
 });
