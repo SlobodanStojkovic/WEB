4562aa6fda6a9d24ad1953fd6ee1b413f74da886
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-06-12 13:12:30 +0200

    Super Mario completed

--- SuperMarioExercise/scriptSuperMario.js
+++ SuperMarioExercise/scriptSuperMario.js
@@ -1,25 +1,28 @@
+var marioImg = document.querySelector("img");
 var bodyNode = document.querySelector("body");
-var marioImg = document.createElement("img");
-marioImg.src = "./assets/mario.png";
-bodyNode.appendChild(marioImg);
 
 var marioRun = document.createElement("img");
 marioRun.src = "./assets/mario_running.gif";
 marioRun.className = "run";
 
-var backgroundImage = document.querySelector("container");
-
+var num = 1;
 //document.querySelector("html").oncontextmenu =    or the method bellow vvv 
 
-function getMarioToRun() {
-    bodyNode.removeChild(marioImg);
-    bodyNode.appendChild(marioRun);
-    setInterval(function () {
-        if (marioRun.style.left != "100%") {
-            marioRun.style.left = (40 + setInterval(function () { var num = 1; num++; return num }, 100)) + "%";
-            bodyNode.style.backgroundPositionX = (40 - setInterval(function () { var num = 1; num++; return num }, 1)) + "vh";
-        }
-    }, 100)
+function getMarioToRun(event) {
+    if (event.key == "ArrowRight") {
+        bodyNode.removeChild(marioImg);
+        bodyNode.appendChild(marioRun);
+        setInterval(function () {
+            //if (marioRun.style.left != "100%") {
+            /* marioRun.style.left = (40 + num) + "%"; */  //this code sets mario run right out of screen
+            bodyNode.style.backgroundPositionX = (40 - num) + "vh";
+            //}
+            num += 2;
+        }, 100)
+    }
 }
 
-document.addEventListener("contextmenu", getMarioToRun);     // if we comment this we need to uncomment code UP 
+bodyNode.addEventListener("keydown", getMarioToRun);     // if we comment this we need to uncomment code UP 
+
+
+
