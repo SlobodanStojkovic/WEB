ee1624bb3f3536ad568c5b1124fbd8b3f225bc78
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-06-16 23:58:41 +0200

    jQuery Inserting Exercise completed bonus condition

--- jQueryInsertingExercise/script1.js
+++ jQueryInsertingExercise/script1.js
@@ -6,6 +6,10 @@ All links to images in the gallery should be elements of the same array!
 When the gallery is created and visible on the page, create gallery title and
 insert it before the gallery images
 Go through gallery images and set a random size to each gallery image
+
+Bonus:
+Set a green border around each image whose width is less than 200px up until the first one that doesn't meet that requirement (i.e. when you get to the first img that is more than 200px wide, stop checking and setting green border).
+
 */
 var imagesArray = ["https://a-z-animals.com/media/2021/01/mammals-400x300.jpg", "https://i.natgeofe.com/n/bec7bd50-0d57-4982-aeb5-82e5f8184f89/02-machine-saving-animals-nationalgeographic_1977490.jpg", "https://insideclimatenews.org/wp-content/uploads/2021/01/tiger-bronx-zoo_james-devaney-getty-scaled.jpg"];
 
@@ -34,6 +38,15 @@ $(".gallery img:not(:first-child):not(:last-child)").css("width", "600px"); // :
 $(".gallery img:last-child").css("width", "700px");
 
 
+
+var stop = true;    //this is needed because after it sets border on first image it needs to stop
+
+$("img").each(function () {
+    if ($("img").width() >= 200 && stop == true) {
+        $(this).css("border", "3px solid green")
+        stop = false;
+    }
+});
 
 
 
