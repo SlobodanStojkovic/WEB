79caf4c840dd5c3641dcef11fa9ec6bef798febe
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-06-25 22:02:30 +0200

    Added option to strikethrough a task that is finished

--- jQueryToDoList/script.js
+++ jQueryToDoList/script.js
@@ -3,7 +3,11 @@ var num = 1;
 
 function addNewTask() {
     var inputText = $("input").val();
-    $("<p>").append(num + ". " + inputText).appendTo(".toDoList");
+    $("<p>").append(num + ". " + inputText).
+        appendTo(".toDoList").
+        click(function () {
+            $(this).toggleClass("linethrough");
+        });
     $("input:text").val("")     //this clears out the value of input field
     num++;
 }
