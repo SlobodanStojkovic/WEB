ba8ecc431a2fe2dd52d6c934bc97c794705d3de4
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-06-28 01:48:41 +0200

    Working on remove button

--- jQueryToDoList/script.js
+++ jQueryToDoList/script.js
@@ -10,5 +10,28 @@ function addNewTask() {
         });
     $("input:text").val("")     //this clears out the value of input field
     num++;
+
+    function addDeleteIcon() {
+        var deleteIcon = $('<img />').attr({
+            'id': 'deleteIcon' + num,
+            'src': './assets/deleteIcon.png',
+            'alt': 'Delete icon',
+            'title': 'Delete icon',
+            'width': 250
+        }).appendTo('p:last');  //appends delete icon to last p element on page
+    };
+    addDeleteIcon();
+
 }
+
+function deleteTask() {
+    $("img").click(function () {
+        $(".toDoList").children("p:last").remove()
+    });
+}
+
+
+
+
+
 
