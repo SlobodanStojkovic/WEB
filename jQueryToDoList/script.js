4380779191bf33177b37353a9843efd1cd81bc94
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-06-28 16:35:35 +0200

    jQuery To Do List completed

--- jQueryToDoList/script.js
+++ jQueryToDoList/script.js
@@ -1,34 +1,39 @@
-
 var num = 1;
 
 function addNewTask() {
     var inputText = $("input").val();
-    $("<p>").append(num + ". " + inputText).
-        appendTo(".toDoList").
+    $("<li>").append(inputText).
+        appendTo("ol").
         click(function () {
             $(this).toggleClass("linethrough");
         });
     $("input:text").val("")     //this clears out the value of input field
     num++;
 
-    function addDeleteIcon() {
-        var deleteIcon = $('<img />').attr({
-            'id': 'deleteIcon' + num,
-            'src': './assets/deleteIcon.png',
-            'alt': 'Delete icon',
-            'title': 'Delete icon',
-            'width': 250
-        }).appendTo('p:last');  //appends delete icon to last p element on page
-    };
-    addDeleteIcon();
-
-}
-
-function deleteTask() {
-    $("img").click(function () {
-        $(".toDoList").children("p:last").remove()
+    var deleteIcon = $('<input>').attr({
+        'id': 'deleteIcon' + num,
+        'type': 'image',
+        'src': './assets/deleteIcon.png',
+        'class': 'delete',
+        'alt': 'Delete task'
+    })
+    $('li:last').append(deleteIcon)
+    $(deleteIcon).click(function () {
+        $(this).closest('li').remove();     //deletes li element in which is deleteIcon located
     });
 }
+
+
+$("input").keypress(function (event) {
+    var key = event.which;
+    if (key === 13)  // the enter key code
+    {
+        addNewTask();
+    }
+});
+
+
+
 
 
 
